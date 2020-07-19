ad983ba783492a13b923096eb13e3d786f6a3d84
Ouadie Lahdioui <ouadiesoft@gmail.com>
2020-07-19 03:55:54 +0200

    Add stick header

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -14,4 +14,17 @@ jQuery(function ($) {
         });
     }
 
+    /**-------------------------------------------------
+     *Fixed HEader
+     *----------------------------------------------------**/
+    $(window).on('scroll', function () {
+
+        /**Fixed header**/
+        if ($(window).scrollTop() > 250) {
+            $('.header').addClass('sticky fade_down_effect');
+        } else {
+            $('.header').removeClass('sticky fade_down_effect');
+        }
+    });
+
 });
