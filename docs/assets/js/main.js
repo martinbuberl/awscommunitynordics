e68a5c56c53a2bfc314f9a643b5a0d6762254575
Ouadie Lahdioui <2996203+ouadie-lahdioui@users.noreply.github.com>
2020-08-14 11:09:54 +0200

    Update countdown

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -6,7 +6,7 @@ jQuery(function ($) {
 
     if ($('.countdown').length > 0) {
         $(".countdown").jCounter({
-            date: '28 August 2020 10:00:00',
+            date: '28 September 2020 10:00:00',
             timezone: "Europe/Paris",
             fallback: function () {
                 console.log("count finished!")
