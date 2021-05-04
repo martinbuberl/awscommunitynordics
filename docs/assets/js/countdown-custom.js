d9d7218a9d4e0e03f6b11a42b8657aad75561b81
Ouadie Lahdioui <ouadiesoft@gmail.com>
2021-05-04 17:10:32 +0200

    feat: update event schedule

--- docs/assets/js/countdown-custom.js
+++ docs/assets/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 05, 22, 0)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 06, 05, 0)}); // year, month, date, hour
         });
 });		
 
