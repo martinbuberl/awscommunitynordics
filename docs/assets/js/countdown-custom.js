7e687e12b5a44aff887de9e535f6654ac490d6a9
Ouadie Lahdioui <ouadiesoft@gmail.com>
2021-05-21 20:34:02 +0200

    update date

--- docs/assets/js/countdown-custom.js
+++ docs/assets/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 05, 05, 0)}); // year, monthIndex, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 05, 26, 0)}); // year, monthIndex, date, hour
         });
 });		
 
